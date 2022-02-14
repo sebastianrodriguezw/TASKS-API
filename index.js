382551a7289da9de188470ab0c1faebef54d58fa
SEBASTIANRO5244 <62524667+SEBASTIANRO5244@users.noreply.github.com>
2022-02-13 22:49:18 -0500

    use cors

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 // root route
 app.get('/', (req, res) => {
