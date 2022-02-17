31f93fa40902c9739b6f8856124bebce317ce837
Sebastian <sebastian.rodriguez@wowdesarrollos.com>
2022-02-17 15:59:57 -0500

    with headers

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,10 +4,13 @@ const db_cont = require('../../config/db.config')
 const {promisify} = require('util')
 
 exports.isAuthenticated = async (req, res, next) => {
-    console.log(req.cookies.jwt)
-    if(req.cookies.jwt) {
+    if(req.cookies.jwt) { req.headers['jwt'] = req.cookies.jwt }
+
+    console.log(req.headers)
+
+    if(req.headers['jwt']) {
         try{
-            const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_CREDENTIALS)
+            const decodificada = await promisify(jwt.verify)(req.headers['jwt'], process.env.JWT_CREDENTIALS)
             await User.findOne({ where: { id: decodificada.id } });
             if(!res){return next()}
 
@@ -17,7 +20,10 @@ exports.isAuthenticated = async (req, res, next) => {
 
         } catch(err) { 
             console.log(err)
-            return next()
+            return res.status(404).json({
+                status: 'failed',
+                error: 'Unauthorized'
+              });
         }
     }else{
         return res.status(404).json({
